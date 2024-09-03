7bccf0e076d60cf50e2c7787893f5a72ac755556
51hhh <87711493+51hhh@users.noreply.github.com>
2024-09-04 07:46:32 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -110,6 +110,11 @@ async def send_telegram_message(message):
     payload = f"key={YOUR_KEY}&webhook={WEBHOOK}&message={message}"
     
     headers = {
+        'User-Agent': 'Apifox/1.0.0 (https://apifox.com)'
+        'Accept': '*/*'
+        'Host'：'	ip.ziyourufeng.eu.org:2000'
+        'Accept-Encoding':'gzip, deflate, br'
+        'Connection':'keep-alive'
         'Content-Type': 'application/json'
     }
     try:
