6b8b9f7b9d224d4533b56c71ddc8671b76f94710
51hhh <87711493+51hhh@users.noreply.github.com>
2024-09-04 15:07:00 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -114,7 +114,7 @@ async def send_telegram_message(message):
     url = URL
     data = {
         "key": "YOUR_SECRET_KEY",
-        "webhook": WEBHOOK,
+        "webhook": f"{WEBHOOK}",
         "message": f"{message}"
     }
     
